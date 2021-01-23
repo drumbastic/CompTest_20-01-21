ace55e9e56c9b0cc8741cb7d00661c4ad38c0bbb
drumbastic <drumbastic@hotmail.com>
2021-01-23 21:38:34 +1100

    Update PluginEditor.cpp

--- PluginEditor.cpp
+++ PluginEditor.cpp
@@ -16,32 +16,32 @@ NewProjectAudioProcessorEditor::NewProjectAudioProcessorEditor (NewProjectAudioP
     // Instaniate knobs, make visible and set styles
     addAndMakeVisible(thresholdKnob = new juce::Slider("Threshold"));
     thresholdKnob->setSliderStyle(juce::Slider::Rotary);
-    thresholdKnob->setTextBoxStyle(juce::Slider::NoTextBox,false,100,100);
+    thresholdKnob->setTextBoxStyle(juce::Slider::TextBoxAbove, true, 100, 20);
     thresholdKnob->setPopupDisplayEnabled(false, true, this);
     thresholdKnob->setTextValueSuffix(" dB");
 
     addAndMakeVisible(ratioKnob = new juce::Slider("Ratio"));
     ratioKnob->setSliderStyle(juce::Slider::Rotary);
-    ratioKnob->setTextBoxStyle(juce::Slider::NoTextBox, false, 100, 100);
+    ratioKnob->setTextBoxStyle(juce::Slider::TextBoxAbove, true, 100, 20);
     ratioKnob->setPopupDisplayEnabled(false,true,this);
     ratioKnob->setTextValueSuffix(":1");
 
     addAndMakeVisible(attackKnob = new juce::Slider("Attack"));
     attackKnob->setSliderStyle(juce::Slider::Rotary);
-    attackKnob->setTextBoxStyle(juce::Slider::NoTextBox, false, 100, 100);
+    attackKnob->setTextBoxStyle(juce::Slider::TextBoxAbove, true, 100, 20);
     attackKnob->setPopupDisplayEnabled(false, true, this);
     attackKnob->setTextValueSuffix("ms");
 
 
     addAndMakeVisible(releaseKnob = new juce::Slider("Release"));
     releaseKnob->setSliderStyle(juce::Slider::Rotary);
-    releaseKnob->setTextBoxStyle(juce::Slider::NoTextBox, false, 100, 100);
+    releaseKnob->setTextBoxStyle(juce::Slider::TextBoxAbove, true, 100, 20);
     releaseKnob->setPopupDisplayEnabled(false, true, this);
     releaseKnob->setTextValueSuffix("ms");
 
     addAndMakeVisible(gainKnob = new juce::Slider("Gain"));
     gainKnob->setSliderStyle(juce::Slider::Rotary);
-    gainKnob->setTextBoxStyle(juce::Slider::NoTextBox, false, 100, 100);
+    gainKnob->setTextBoxStyle(juce::Slider::TextBoxAbove, true, 100, 20);
     gainKnob->setPopupDisplayEnabled(false, true, this);
     gainKnob->setTextValueSuffix("dB");
 
@@ -75,6 +75,8 @@ void NewProjectAudioProcessorEditor::paint (juce::Graphics& g)
     g.drawFittedText("Attack",    ((getWidth() / 6) * 4) - (100 / 2), (getHeight() / 2) + 5, 100, 100, juce::Justification::centred, false);
     g.drawFittedText("Release",   ((getWidth() / 6) * 5) - (100 / 2), (getHeight() / 2) + 5, 100, 100, juce::Justification::centred, false);
     g.drawFittedText("Gain",      ((getWidth() / 6) * 1) - (100 / 2), (getHeight() / 2) + 5, 100, 100, juce::Justification::centred, false);
+
+    //g.drawFittedText(, ((getWidth() / 6) * 1) - (100 / 2), (getHeight() / 2) - 50, 100, 100, juce::Justification::centred, false);
 }
 
 void NewProjectAudioProcessorEditor::resized()
